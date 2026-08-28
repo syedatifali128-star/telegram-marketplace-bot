2d10e797ce009a6ad1a012581d845f67f672ae51
syedatifali128-star <syedatifali128@gmail.com>
2026-08-28 14:23:26 +0530

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -100,10 +100,10 @@ async def send_advertisements():
                 else:    
                     print(f"[-] Failed to send to {group}: {e}")
 
-            await asyncio.sleep(60)
+               await asyncio.sleep(60)
 
-        print("\nWaiting 1 hour for the next broadcast round...\n")
-        await asyncio.sleep(3600)
+           print("\nWaiting 1 hour for the next broadcast round...\n")
+           await asyncio.sleep(3600)
 
 
 
